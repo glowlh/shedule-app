d7f4b0f4583b5e69d5b2bdce95745df2ae0b4daa
glowlh <m.o.gushchina@gmail.com>
2017-10-01 11:45:15 +0300

    add validation for checking unique store item

--- src/components/classroom/services.js
+++ src/components/classroom/services.js
@@ -2,7 +2,13 @@ import ClassroomValidator from './validator';
 import {addClassroom as addClassroomAction} from './actions';
 
 export const addClassroom = (payload) => {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const classrooms = getState().classrooms;
+    const isUnique = !classrooms.some(it => it.name === payload.name);
+    if (!isUnique) {
+      return Promise.reject({error: 'The classroom is not unique'});
+    }
+
     const classroomValidator = new ClassroomValidator();
     return classroomValidator.validate(payload)
       .then(() => {
